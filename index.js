c50e69bcd49975bf75cba456a21a56c8803dbd53
+ <diazcristina.la@gmail.com>
2022-04-18 12:17:27 -0700

    commit tree b22c453a625c76606779e1ef0535780fbda73591

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const message = [];
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+}
